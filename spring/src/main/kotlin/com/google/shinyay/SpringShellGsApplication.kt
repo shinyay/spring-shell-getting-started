817946fb67e74e701481b6bf28f6c613b7c79583
shinyay <shinya.com@gmail.com>
2020-04-22 22:33:03 +0900

    Modify typo

--- spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
+++ spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
@@ -4,9 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.shell.standard.ShellComponent
-import org.springframework.shell.standard.ShellMethod
-spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
+
 @SpringBootApplication
 class SpringShellGsApplication
 
