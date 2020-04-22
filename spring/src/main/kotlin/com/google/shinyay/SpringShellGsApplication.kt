9f20334a63dab1504d20bf4981f9642a9bd51de1
shinyay <shinya.com@gmail.com>
2020-04-22 22:23:59 +0900

    Modify shell command class

--- spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
+++ spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
@@ -1,17 +1,18 @@
 package com.google.shinyay
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
-
-@ShellComponent
+spring/src/main/kotlin/com/google/shinyay/SpringShellGsApplication.kt
 @SpringBootApplication
-class SpringShellGsApplication {
-	@ShellMethod("Sample Command")
-	fun helloWorld(): String = "Hello World"
-}
+class SpringShellGsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringShellGsApplication>(*args)
+    runApplication<SpringShellGsApplication>(*args)
 }
+
+val Any.logger: Logger
+    get() = LoggerFactory.getLogger(this.javaClass)
